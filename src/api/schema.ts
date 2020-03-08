13a8a1c2a6e7d8eae690817b7ee53b7cea41a684
Boccochan <boccochan@gmail.com>
2020-03-08 15:16:15 +0900

    Add login function

--- src/api/schema.ts
+++ src/api/schema.ts
@@ -1,12 +1,13 @@
 import { buildSchema } from "type-graphql";
 
+import { Login } from "./resolver/Login";
 import { Logout } from "./resolver/Logout";
 import { Me } from "./resolver/Me";
 import { Register } from "./resolver/Register";
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [Me, Register, Logout],
+    resolvers: [Me, Register, Logout, Login],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     }
